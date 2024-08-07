ebfa4385bf559b74e8e48aaff6e11ef98d2bffa3
Avaray <1459553+Avaray@users.noreply.github.com>
2024-08-07 21:53:00 +0200

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+dotnet publish -c Release -r win-x64 -p:PublishSingleFile=true -p:SelfContained=true
+dotnet publish -c Release -r win-x86 -p:PublishSingleFile=true -p:SelfContained=true
 dotnet publish -c Release -r linux-x64 -p:PublishSingleFile=true -p:SelfContained=true
+dotnet publish -c Release -r linux-x86 -p:PublishSingleFile=true -p:SelfContained=true
+dotnet publish -c Release -r osx-x64 -p:PublishSingleFile=true -p:SelfContained=true
+dotnet publish -c Release -r osx-x86 -p:PublishSingleFile=true -p:SelfContained=true
