96beaaf29a6033f4bbc6894579e119713a49baac
Avaray <1459553+Avaray@users.noreply.github.com>
2024-08-08 07:46:17 +0200

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# This file is used to manually build executables for all platforms.
+# For GitHub Actions you can specify the platforms in jobs.build.strategy.matrix
+
 dotnet publish -c Release -r win-x64 -p:PublishSingleFile=true -p:SelfContained=true
 dotnet publish -c Release -r win-x86 -p:PublishSingleFile=true -p:SelfContained=true
 dotnet publish -c Release -r linux-x64 -p:PublishSingleFile=true -p:SelfContained=true
